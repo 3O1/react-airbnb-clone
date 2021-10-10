6c882330e9872eabd86aa7694e69fb040a545ea4
3O1 <jamesbrandonedwards301@gmail.com>
2021-10-10 10:23:03 -0400

    Added 1st section of header

--- web/components/Header.jsx
+++ web/components/Header.jsx
@@ -1,13 +1,17 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 function Header() {
   /** Use header tag for SEO & google */
   return (
-    <header>
-      <h1>I am the header</h1>
-
+    /**
+     * breakpoints - mobile first
+     */
+    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-sm p-5 md:px-10 border-b border-gray-200">
       {/* left section */}
-      <div>
+
+      {/* relative - relative to container
+       */}
+      <div className="relative flex items-center h-10 cursor-pointer my-auto">
         {/* use Next.js img tag compresses & optimized
          * needs to know dimensions to compress
          * to prepare next.js images -> whitelist domains
@@ -15,11 +19,15 @@ function Header() {
         <Image
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/2560px-Airbnb_Logo_B%C3%A9lo.svg.png"
           layout="fill"
+          objectFit="contain"
+          objectPosition="left"
         ></Image>
       </div>
 
       {/* middle section */}
-      <div></div>
+      <div>
+        <input type="text" />
+      </div>
 
       {/* right section */}
       <div></div>
