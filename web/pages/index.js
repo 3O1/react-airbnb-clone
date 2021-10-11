50decd26677b714039fb0414f19e8c65fc6a2e76
3O1 <jamesbrandonedwards301@gmail.com>
2021-10-11 03:33:19 -0400

    Added MediumCard

--- web/pages/index.js
+++ web/pages/index.js
@@ -2,10 +2,11 @@ import Head from 'next/head';
 import Header from '../components/Header';
 import Banner from '../components/Banner';
 import SmallCard from '../components/SmallCard';
+import MediumCard from '../components/MediumCard';
 
 /** Represents home / */
 
-export default function Home({ exploreData }) {
+export default function Home({ exploreData, liveAnywhereData }) {
   return (
     <div className="">
       <Head>
@@ -19,7 +20,7 @@ export default function Home({ exploreData }) {
 
       <main className="px-8 mx-auto max-w-7xl sm:px-16">
         <section className="pt-6">
-          <h2 className="pb-5 text-4xl font-semibold">Explore nearby</h2>
+          <h2 className="pb-5 text-3xl font-semibold">Explore nearby</h2>
 
           {/* API
            * make optional to protect
@@ -27,14 +28,26 @@ export default function Home({ exploreData }) {
            * set the `SmallCard` props to the item
            */}
 
-          {exploreData?.map(({ img, distance, location }) => (
-            <SmallCard
-              key={img}
-              img={img}
-              distance={distance}
-              location={location}
-            />
-          ))}
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {exploreData?.map(({ img, distance, location }) => (
+              <SmallCard
+                key={img}
+                img={img}
+                distance={distance}
+                location={location}
+              />
+            ))}
+          </div>
+        </section>
+
+        {/* Live anywhere */}
+        <section>
+          <h2 className="py-8 text-3xl font-semibold">Live anywhere</h2>
+          <div className="flex space-x-3 overflow-scroll">
+            {liveAnywhereData?.map(({ img, title }) => (
+              <MediumCard key={img} img={img} title={title} />
+            ))}
+          </div>
         </section>
       </main>
     </div>
@@ -46,9 +59,14 @@ export async function getStaticProps() {
     (res) => res.json()
   );
 
+  const liveAnywhereData = await fetch('https://links.papareact.com/zp1').then(
+    (res) => res.json()
+  );
+
   return {
     props: {
       exploreData,
+      liveAnywhereData,
     },
   };
 }
