6e7a349e8836434055aff1cbceba2e07e57873b4
3O1 <jamesbrandonedwards301@gmail.com>
2021-10-11 07:43:49 -0400

    Added progress bar

--- web/components/InfoCard.jsx
+++ web/components/InfoCard.jsx
@@ -4,9 +4,14 @@ import { StarIcon } from '@heroicons/react/solid';
 
 function InfoCard({ img, location, title, description, star, price, total }) {
   return (
-    <div className="flex px-2 border-t cursor-pointer py-7 hover:opacity-80">
+    <div className="flex px-2 pr-2 border-b cursor-pointer py-7 first:border-t">
       <div className="relative flex-shrink-0 w-40 h-24 md:h-52 md:w-80">
-        <Image src={img} layout="fill" objectFit="cover" />
+        <Image
+          className="rounded-md sm:rounded-lg"
+          src={img}
+          layout="fill"
+          objectFit="cover"
+        />
       </div>
 
       <div className="flex flex-col flex-grow pl-5">
